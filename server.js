33f6b0058cfe45628ae20eb38e4fdf87d40e1876
Larry Hernandez <larryhernanjr@gmail.com>
2017-01-07 18:33:39 -0500

    add REST routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 
 const app = express();
 
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157298.mlab.com:57298/todoangular');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -14,6 +14,52 @@ app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride);
+
+const Todo = mongoose.model('Todo', {
+  text : String
+});
+
+app.get('/api/todos', (req,res) => {
+  Todo.find((err, todos) => {
+    if(err) {
+      res.send(err);
+    };
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', (req,res) => {
+  Todo.create({
+    text: req.body.text,
+    done: false
+  }, (err, todo) => {
+    if(err) {
+      res.send(err);
+    };
+    Todo.find((err, todos) => {
+      if(err) {
+        res.send(err);
+      };
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/todos/:todo_id', (req, res) => {
+  Todo.remove({
+    _id: req.params.todo_id
+  }, (err, todo) => {
+      if(err) {
+        res.send(err);
+      };
+      Todo.find((err, todos) => {
+        if(err) {
+          res.send(err);
+        }
+        res.json(todos);
+      });
+  });
+});
 
 app.listen(8080);
 console.log('App listening on port 8080');
