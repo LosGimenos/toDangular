002ea8de27be32958c38e20faeed5a30904ba86e
Larry Hernandez <larryhernanjr@gmail.com>
2017-01-07 18:56:35 -0500

    add route for index

--- server.js
+++ server.js
@@ -61,5 +61,9 @@ app.delete('/api/todos/:todo_id', (req, res) => {
   });
 });
 
+app.get('*', (req,res)=>{
+  res.sendfile('./public/index.html');
+});
+
 app.listen(8080);
 console.log('App listening on port 8080');
